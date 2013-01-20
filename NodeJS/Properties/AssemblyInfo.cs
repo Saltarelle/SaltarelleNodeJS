079618f9e5f6ad876db9dabad7d862efc03ab26c
Erik Källén <erik_kallen@hotmail.com>
2013-01-20 19:26:59 +0100

    Build works

---
+++ NodeJS/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿// AssemblyInfo.cs
+// Script#/Libraries/Knockout
+// This source code is subject to terms and conditions of the Apache License, Version 2.0.
+//
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Script.NodeJS")]
+[assembly: AssemblyDescription("Saltarelle Node.js API")]
+[assembly: ScriptAssembly("node")]
+
+[assembly: AssemblyCompany("Erik Källén")]
+[assembly: AssemblyProduct("Saltarelle Node.js bindings")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
